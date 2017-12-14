9bea49f1ab7cc4ced05582e7b67b8d6d8e1d0f41
David Lars Ketch <davidlarsketch@gmail.com>
2017-12-14 13:25:57 -0600

    Beautifies code; initializes users.js

--- assets/js/initialize.js
+++ assets/js/initialize.js
@@ -1,11 +1,25 @@
 "use strict";
 const listeners = require('./events');
+const setLocalStorage = require("./set-local-storage");
 const getLocalStorage = require("./get-local-storage");
+const testMsg = [
+    {"body": "Nunc nisi adipiscing pellentesque lacus? Platea vel lundium facilisis",
+    "timestamp": 1513108861000,
+    "user": 1}
+];
 
+const testUser = [{
+    "id": 1,
+    "username": "luke"
+}];
 const init = () => {
+
+    setLocalStorage.save("messages", testMsg);
+    setLocalStorage.save("users", testUser);
+
     getLocalStorage.fetch("users");
-    getLocalStorage.fetch("themes");
-    getLocalStorage.fetch("sizer");
+    // getLocalStorage.fetch("themes");
+    // getLocalStorage.fetch("sizer");
     getLocalStorage.fetch("messages");
     listeners.addListeners();
 };
