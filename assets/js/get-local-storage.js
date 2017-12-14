e1150ded1a8e1a55e79ab75962f9e438f838b6c3
David Lars Ketch <davidlarsketch@gmail.com>
2017-12-14 12:56:25 -0600

    Comments out calls for settings-router until that mod is completed

--- assets/js/get-local-storage.js
+++ assets/js/get-local-storage.js
@@ -1,12 +1,12 @@
 "use strict";
 const msgs = require("./messages-router");
-const settings = require("./settings-router");
+// const settings = require("./settings-router");
 
 const modFuncs = {
     messages: msgs.routeFetch,
     users: msgs.routeFetch,
-    themes: settings.testLog,
-    sizer: settings.testLog,
+    // themes: settings.testLog,
+    // sizer: settings.testLog,
 };
 
 let itemsArr = [];
