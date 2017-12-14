8212e3f8f78655f83d025f03c363d41d33a10ba6
David Lars Ketch <davidlarsketch@gmail.com>
2017-12-14 12:54:50 -0600

    Adds get-local-storage.js mod

---
+++ assets/js/get-local-storage.js
@@ -0,0 +1,17 @@
+"use strict";
+const msgs = require("./messages-router");
+const settings = require("./settings-router");
+
+const modFuncs = {
+    messages: msgs.routeFetch,
+    users: msgs.routeFetch,
+    themes: settings.testLog,
+    sizer: settings.testLog,
+};
+
+let itemsArr = [];
+
+module.exports.fetch = key => {
+    itemsArr = JSON.parse(localStorage.getItem(key));
+    modFuncs[key](key, itemsArr);
+};
